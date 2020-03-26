4fbfcf087316309d25e4f83f03ab4c6ec85efc56
ian and laura Wolstenholme <ian.wolstenholme33@gmail.com>
2020-03-26 12:55:17 +0000

    Initial Test Created and passed Refactor to be done

--- Kata/KataTest/UnitTest1.cs
+++ Kata/KataTest/UnitTest1.cs
@@ -1,18 +1,22 @@
+using Kata.Classes;
+using Kata.Model;
 using NUnit.Framework;
 
 namespace KataTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        private const decimal A99_QTY_THREE_TOTAL = 1.3m;
 
         [Test]
-        public void Test1()
+        public void TestDiscountSuccuss()
         {
-            Assert.Pass();
+            Item _item = new Item();
+            _item.Product = "A99";
+            _item.Quantity = 3;
+            var _scan = new Scanner();
+            decimal _totalprice = _scan.GetPrice(_item);
+            Assert.AreEqual(_totalprice, A99_QTY_THREE_TOTAL);
         }
     }
 }
