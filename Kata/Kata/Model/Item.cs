0f7db0f13ce3b872ca446884392514c15ddce8e1
ian and laura Wolstenholme <ian.wolstenholme33@gmail.com>
2020-03-26 12:10:32 +0000

    Initial commit for the Model

---
+++ Kata/Kata/Model/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.Model
+{
+    public class Item
+    {
+        public string Product { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
