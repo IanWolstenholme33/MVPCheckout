2fd56bd88cdb65edb47ce6b575fd09b6ccc20a92
ian and laura Wolstenholme <ian.wolstenholme33@gmail.com>
2020-03-26 14:17:20 +0000

    introduce catering for all qtys and products initial commit to be fixed

--- Kata/Kata/Factory/PriceFactory.cs
+++ Kata/Kata/Factory/PriceFactory.cs
@@ -17,16 +17,39 @@ namespace Kata.Factory
 
         public decimal GetDiscount()
         {
-            if (_item.Product == ProductEnum.A99 && _item.Quantity == 3)
+            var discountedItems = 0;
+            decimal total = 0m;
+            switch (_item.Product)
             {
-                return Convert.ToDecimal(1.30 / _item.Quantity);
+                case ProductEnum.A99:
+                    discountedItems = _item.Quantity / 3;
+                    if (discountedItems > 0)
+                    {
+                        total = Convert.ToDecimal(1.30 / discountedItems);
+                        var newQty = _item.Quantity - (discountedItems * 3);
+                        return total + Convert.ToDecimal(newQty * 0.50);
+                    }
+                    else
+                    {
+                        return Convert.ToDecimal(_item.Quantity * 0.50);
+                    }
+                case ProductEnum.B15:
+                    discountedItems = _item.Quantity / 2;
+                    if (discountedItems > 0)
+                    {
+                        total = Convert.ToDecimal(0.45 / discountedItems);
+                        var newQty = _item.Quantity - (discountedItems * 2);
+                        return total + Convert.ToDecimal(newQty * 0.45);
+                    }
+                    else
+                    {
+                        return Convert.ToDecimal(_item.Quantity * 0.50);
+                    }
+                case ProductEnum.C40:
+                    return 0m;
+                default:
+                    return 0m;
             }
-            else if (_item.Product == ProductEnum.A99 && _item.Quantity < 3)
-            {
-                return Convert.ToDecimal(0.50 * _item.Quantity);
-            }
-
-            return 0m;
         }
     }
 }
--- Kata/KataTest/UnitTest1.cs
+++ Kata/KataTest/UnitTest1.cs
@@ -8,9 +8,10 @@ namespace KataTest
     public class Tests
     {
         private const decimal A99_QTY_THREE_TOTAL = 1.3m;
+        private const decimal B15_QTY_TWO_TOTAL = 0.45m;
 
         [Test]
-        public void TestDiscountSuccess()
+        public void TestDiscountA99Success()
         {
             Item _item = new Item();
             _item.Product = ProductEnum.A99;
@@ -18,6 +19,26 @@ namespace KataTest
             var _scan = new Scanner();
             decimal _totalprice = _scan.GetPrice(_item);
             Assert.AreEqual(_totalprice, A99_QTY_THREE_TOTAL);
+        }
+
+        [Test]
+        public void TestDiscountNULLError()
+        {
+            Item _item = null;        
+            var _scan = new Scanner();
+            decimal _totalprice = _scan.GetPrice(_item);
+            Assert.AreEqual(_totalprice, 0m);
+        }
+
+        [Test]
+        public void TestDiscountB15Success()
+        {
+            Item _item = new Item();
+            _item.Product = ProductEnum.B15;
+            _item.Quantity = 2;
+            var _scan = new Scanner();
+            decimal _totalprice = _scan.GetPrice(_item);
+            Assert.AreEqual(_totalprice, B15_QTY_TWO_TOTAL);
         }
     }
 }
